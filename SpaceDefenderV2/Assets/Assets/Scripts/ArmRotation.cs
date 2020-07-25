1ed28684a621a6599c8afa429c4917a4fb5f9f7e
MikeyBerger <51145079+MikeyBerger@users.noreply.github.com>
2020-07-25 10:35:43 -0700

    More Asset Management

---
+++ SpaceDefenderV2/Assets/Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour
+{
+    public float Offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Difference.Normalize();
+        float RotZ = Mathf.Atan2(Difference.y, Difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, RotZ + Offset);
+    }
+}
