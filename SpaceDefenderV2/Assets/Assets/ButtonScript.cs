0287d50f8cb91916b9acbb8c38a5ad53cb814a22
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-04 14:03:25 -0700

    Ran Into Bugs Again

--- SpaceDefenderV2/Assets/Assets/ButtonScript.cs
+++ SpaceDefenderV2/Assets/Assets/ButtonScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class ButtonScript : MonoBehaviour
     //public Image ButtonImage;
  
     public string MainMenu;
+    public Button ThisButton;
+    public RectTransform Rect;
     
 
     // Start is called before the first frame update
@@ -24,20 +27,31 @@ public class ButtonScript : MonoBehaviour
         
     }
 
-    private void OnMouseEnter()
+    public void OnMouseEnter()
     {
-        transform.localScale = transform.localScale * ScaleSize;
-        
+        transform.localScale = new Vector3(ScaleSize, ScaleSize, 0);
+        Rect.localScale = new Vector3(ScaleSize, ScaleSize, 0);
+
     }
 
-    private void OnMouseExit()
+    public void OnMouseExit()
     {
-        transform.localScale = transform.localScale / ScaleSize;
-        
+        transform.localScale = new Vector3(1, 1, 0);
+        Rect.localScale = new Vector3(1, 1, 0);
+
     }
 
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         SceneManager.LoadScene(MainMenu);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Mouse")
+        {
+            transform.localScale = new Vector3(ScaleSize, ScaleSize, 0);
+            Rect.localScale = new Vector3(ScaleSize, ScaleSize, 0);
+        }
     }
 }
